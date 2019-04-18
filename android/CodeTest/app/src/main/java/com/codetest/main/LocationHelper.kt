56ffea5d8774303ccfdf916546c052dd658bd90b
ruhiya <ruhiya@kry.se>
2019-04-18 15:34:31 +0200

    remove adding locations

--- android/CodeTest/app/src/main/java/com/codetest/main/LocationHelper.kt
+++ android/CodeTest/app/src/main/java/com/codetest/main/LocationHelper.kt
@@ -2,12 +2,12 @@ package com.codetest.main
 
 import com.codetest.main.api.LocationApiService
 import com.codetest.main.model.Location
-import com.codetest.main.model.Status
 import java.util.*
 
 class LocationHelper {
 
     companion object {
+
         fun getLocations(callback: (List<Location>?) -> Unit) {
             val locations: ArrayList<Location> = arrayListOf()
             val apiKey = KeyUtil().getKey()
@@ -21,23 +21,5 @@ class LocationHelper {
                 callback(null)
             })
         }
-
-        fun addLocation(name: String, callback: (Boolean) -> Unit) {
-            val apiKey = KeyUtil().getKey()
-
-            val location = Location(
-                UUID.randomUUID().toString(),
-                name,
-                (-10..45).shuffled().first().toString(),
-                Status.values().toList().shuffled().first()
-            )
-
-            LocationApiService.getApi().post(apiKey, "locations", location, {
-                callback(true)
-            }, {
-                callback(false)
-            })
-        }
     }
-
 }
--- android/CodeTest/app/src/main/java/com/codetest/main/WeatherForecastActivity.kt
+++ android/CodeTest/app/src/main/java/com/codetest/main/WeatherForecastActivity.kt
@@ -2,20 +2,17 @@ package com.codetest.main
 
 import android.app.AlertDialog
 import android.os.Bundle
-import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.view.ViewGroup
+import com.codetest.R
 import com.codetest.main.model.Location
-import com.codetest.main.ui.AddLocationDialogFragment
 import com.codetest.main.ui.LocationViewHolder
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class WeatherForecastActivity : AppCompatActivity(), View.OnClickListener, AddLocationDialogFragment.Listener,
-    SwipeRefreshLayout.OnRefreshListener {
+class WeatherForecastActivity : AppCompatActivity() {
 
     private var adapter = ListAdapter()
     private var list: List<Location> = arrayListOf()
@@ -24,35 +21,11 @@ class WeatherForecastActivity : AppCompatActivity(), View.OnClickListener, AddLo
         super.onCreate(savedInstanceState)
         setContentView(com.codetest.R.layout.activity_main)
 
-        swipeLayout?.setOnRefreshListener(this)
-        swipeLayout?.isEnabled = true
-
         adapter = ListAdapter()
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.adapter = adapter
         adapter.notifyDataSetChanged()
-        actionButton.setOnClickListener(this)
         fetchLocations()
-    }
-
-    override fun onRefresh() {
-        fetchLocations()
-    }
-
-    override fun onClick(v: View?) {
-        AddLocationDialogFragment.show(this.supportFragmentManager)
-    }
-
-    override fun onSimpleDialogResult(value: String) {
-        LocationHelper.addLocation(value) {
-            if (it) {
-                fetchLocations()
-
-            } else {
-                showError()
-
-            }
-        }
     }
 
     private fun fetchLocations() {
@@ -60,10 +33,6 @@ class WeatherForecastActivity : AppCompatActivity(), View.OnClickListener, AddLo
             if (response == null) {
                 showError()
             } else {
-                if (swipeLayout.isRefreshing) {
-                    swipeLayout.isRefreshing = false
-                }
-
                 list = response
                 adapter.notifyDataSetChanged()
             }
@@ -72,9 +41,9 @@ class WeatherForecastActivity : AppCompatActivity(), View.OnClickListener, AddLo
 
     private fun showError() {
         AlertDialog.Builder(this)
-            .setTitle("Error")
-            .setMessage("Something went wrong")
-            .setPositiveButton("Ok", { _, _ -> })
+            .setTitle(resources.getString(R.string.error_title))
+            .setMessage(resources.getString(R.string.error_title))
+            .setPositiveButton(resources.getString(R.string.ok), { _, _ -> })
             .create()
             .show()
     }
